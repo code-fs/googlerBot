6c1ea3faf19f33db7e4871d163345ba741e9a85f
LAVIESTES <73939435+code-fs@users.noreply.github.com>
2022-04-09 23:01:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ async def on_ready():
 @bot.command()
 async def ggl(ctx, *, search):
   async with aiohttp.ClientSession() as session:
-    async with session.get(f"https://www.googleapis.com/customsearch/v1?key={os.envrion["API_KEY"]}&cx=e367fa34ef717987e&q={search}") as resp:
+    async with session.get(f"https://www.googleapis.com/customsearch/v1?key={os.environ["API_KEY"]}&cx=e367fa34ef717987e&q={search}") as resp:
       js = resp.json()
     
       embed = discord.Embed(
@@ -26,4 +26,4 @@ async def ggl(ctx, *, search):
     
       await ctx.send(embed=embed)
     
-bot.run(os.envrion["DISCORD_TOKEN"])
+bot.run(os.environ["DISCORD_TOKEN"])
